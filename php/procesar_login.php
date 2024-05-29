733b90ecb7ecbca6a77d491a667245c12bff79c5
AxlitoInArg <axel.albarenque.t1vl@gmail.com>
2024-05-29 16:28:49 -0300

    arreglo de procesado de login

--- php/procesar_login.php
+++ php/procesar_login.php
@@ -48,7 +48,7 @@
     ?>
 
     <div class="login-container">
-        <h2>Logueo - CallNex</h2>
+        <h2>Redirigiendo</h2>
         <?php if ($login_success): ?>
             <p class="success-message">Inicio de sesión exitoso. Redirigiendo...</p>
             <script>
@@ -57,21 +57,12 @@
                 }, 3000); // Redirigir después de 3 segundos
             </script>
         <?php else: ?>
-            <form id="loginForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <div class="input-group">
-                    <label for="email">Correo electrónico:</label>
-                    <input type="email" id="email" name="email" required>
-                </div>
-                <div class="input-group">
-                    <label for="password">Contraseña:</label>
-                    <input type="password" id="password" name="password" required>
-                </div>
-                <?php if (!empty($error_message)): ?>
-                    <p class="error-message"><?php echo $error_message; ?></p>
-                <?php endif; ?>
-                <button type="submit" class="btn">Iniciar sesión</button>
-            </form>
-            <p>No tienes una cuenta? <a href="http://localhost/callnex/php/registro.php">Regístrate aquí</a></p>
+            <p class="error-message">ERROR, VOLVIENDO AL LOGUEO</p>
+            <script>
+                setTimeout(function() {
+                    window.location.href = '/callnex/html/login.html'; // Cambiar a la página de inicio correspondiente
+                }, 3000); // Redirigir después de 3 segundos
+            </script>
         <?php endif; ?>
     </div>
 
